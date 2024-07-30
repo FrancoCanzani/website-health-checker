858c9f76cb92c4c9cea2383cff92dd490f6525bb
Franco Canzani <francocanzani@gmail.com>
2024-07-30 22:12:05 +0200

    Add project files.

---
+++ HealthChecker/Controllers/HealthCheckController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HealthChecker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HealthCheckController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CheckHealth([FromQuery] string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return BadRequest("URL parameter is required");
+            }
+
+            // We'll implement the health check logic in the next step
+            bool isHealthy = await CheckWebsiteHealth(url);
+
+            return Ok(new { url = url, isHealthy = isHealthy });
+        }
+
+        private async Task<bool> CheckWebsiteHealth(string url)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                client.Timeout = TimeSpan.FromSeconds(30); // Set a timeout
+
+                var response = await client.GetAsync(url);
+                return response.IsSuccessStatusCode;
+            }
+            catch (HttpRequestException)
+            {
+                return false;
+            }
+            catch (TaskCanceledException)
+            {
+                return false; // Timeout occurred
+            }
+        }
+    }
+}
